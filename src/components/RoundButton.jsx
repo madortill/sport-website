c4868cf3b5bf1b6f604f530ac7266d65097b8354
madorTill <mador.till@gmail.com>
2025-07-07 17:25:24 +0300

    deploy

--- src/components/RoundButton.jsx
+++ src/components/RoundButton.jsx
@@ -8,15 +8,10 @@ function RoundButton({groupName}) {
     return <p>לא נמצאו כפתורים עבור הקבוצה "{groupName}"</p>;
   }
 
-  const handleClick = () => {
-      
-  }
-
-
   return (
     <div className="roundButtons">
       {buttons.map((btn, i) => (
-        <button onClick={handleClick} key={i} className={`info-button info-button-${btn.icon}`}>
+        <button key={i} className={`info-button info-button-${btn.icon}`}>
           <img
             src={`/src/assets/images/logos/${btn.icon}-logo.svg`}
             alt={btn.text}
